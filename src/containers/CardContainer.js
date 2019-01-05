e6cb7ccf3e7db31c14a37f8017f6b1440c0d9936
Mike Duke <aithon@gmail.com>
2019-01-05 16:27:59 -0700

    Fetch working in CardContainer and very basic cards are on the page

---
+++ src/containers/CardContainer.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Card } from '../components/Card';
+import apiKey from '../apiKey';
+import { fetchArticles } from '../thunks/fetchArticles';
+
+class CardContainer extends Component {
+
+  componentDidMount() {
+    const url = `https://newsapi.org/v2/everything?q=+parents AND (kid OR kid OR child OR children) AND ${this.props.topic}&pageSize=20&language=en&sortBy=relevancy&apiKey=${apiKey}`;
+    this.props.addArticlesToStore(url);
+  }
+  
+  render() {
+    const articlesToDisplay = this.props.articles.map((article) => {
+      return <Card article={article} />
+    })
+    // console.log(this.props.articles)
+    return (
+      <section className="card-container">
+        <h1>CardContainer</h1>
+        {articlesToDisplay}
+      </section>
+    )
+  }
+}
+
+export const mapStateToProps = (state) => ({
+  articles: state.articles
+})
+
+export const mapDispatchToProps = (dispatch) => ({
+  addArticlesToStore: (url) => dispatch(fetchArticles(url))
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(CardContainer);
--- src/thunks/fetchArticles.js
+++ src/thunks/fetchArticles.js
@@ -8,6 +8,7 @@ export const fetchArticles = (url) => {
         console.log(response.statusText);
       }
       const result = await response.json();
+      console.log(result.articles);
       dispatch(addArticles(result.articles));
     } catch (error) {
       console.log(error);
