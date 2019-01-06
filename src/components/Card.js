9e7a98347d024c2d55b909f0dc77c6b2d8257ae7
Mike Duke <aithon@gmail.com>
2019-01-05 17:44:34 -0700

    Cards look a little better

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,14 +1,13 @@
 import React from 'react';
 
 export const Card = ({article}) => {
-  const { urlToImage, title } = article
+  const { urlToImage, title, author } = article
   return (
     <article className="card">
-      <div className="image-container">
-        <img src={urlToImage} alt={title} />
+      <div className="image-container" style={{backgroundImage: `URL(${urlToImage})`}}>
       </div>
       <h3>{title}</h3>
-      <p>author</p>
+      <p>By {author}</p>
     </article>
   )
 }
