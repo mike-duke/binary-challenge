762f347749f3ed2e1200f466e194c76c2c2e09c2
Mike Duke <aithon@gmail.com>
2019-01-09 10:33:31 -0700

    Fix tests

--- src/components/__tests__/Card.js
+++ src/components/__tests__/Card.js
@@ -4,7 +4,7 @@ import { Card } from '../Card';
 
 describe('Card', () => {
   it('should match the snapshot', () => {
-    const wrapper = shallow(<Card article={{title: 'Cats are cool', author: 'All the cats'}} />);
+    const wrapper = shallow(<Card article={{title: 'Cats are cool', author: 'All the cats', source: {name: 'Cat News'}}} />);
 
     expect(wrapper).toMatchSnapshot();
   });
